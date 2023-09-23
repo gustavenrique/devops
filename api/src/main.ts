887e28bc723d2b5ca608b5a5be85f59668e1082e
Gustavo H. S. Oliveira <gustax.dev@gmail.com>
2023-09-23 10:51:44 -0300

    Containerizing all components

--- api/src/main.ts
+++ api/src/main.ts
@@ -20,7 +20,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/values', valuesRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
 	console.log(
